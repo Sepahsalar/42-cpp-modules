d09d250ec1c64c4b9841d658175ccc2f64ff4d5b
Alireza Sohrabizadeh <alirezasohrabizadeh@gmail.com>
2024-09-04 23:16:08 +0300

    Add Makefile and main.cpp for cpp01/ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 19:25:36 by asohrabi          #+#    #+#             */
+/*   Updated: 2024/09/04 23:15:24 by asohrabi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int	main()
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "Address of string      : " << &string << std::endl;
+	std::cout << "Address -> by pointer  : " << stringPTR << std::endl;
+	std::cout << "Address -> by reference: " << &stringREF << std::endl;
+	
+	std::cout << "Value of string        : " << string << std::endl;
+	std::cout << "Value -> by pointer    : " << *stringPTR << std::endl;
+	std::cout << "Value -> by reference  : " << stringREF << std::endl;
+	return 0;
+}
